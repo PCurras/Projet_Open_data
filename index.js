e2b12c7e2ee8b8081a54864f56df60839a5ad4ea
Baptiste-Bo <baptbonneau@yahoo.fr>
2019-11-05 10:54:08 +0100

    'API_pays'

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ app.get('/', function (req, res) {
     res.send('Hello, vous êtes à la racine de ce serveur ! allez voir /index')
   })
 
+var http = require("https");
+
+var options = {
+	"method": "GET",
+	"hostname": "ajayakv-rest-countries-v1.p.rapidapi.com",
+	"port": null,
+	"path": "/rest/v1/all",
+	"headers": {
+		"x-rapidapi-host": "ajayakv-rest-countries-v1.p.rapidapi.com",
+		"x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+	}
+};
+
+var req = http.request(options, function (res) {
+	var chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
+
+
 //API
 
 /* app.get('/user/:name', function(req, res) {
@@ -20,7 +49,7 @@ app.get('/', function (req, res) {
 	}
 }) */
 
-app.get('/names', function(req,res) {
+/* app.get('/names', function(req,res) {
 	res.format({
         'application/json': function () {
             res.json([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
@@ -30,11 +59,11 @@ app.get('/names', function(req,res) {
             res.csv([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
         }
     })
-})
+}) */
 
 //static ressources
 
-app.get('/index', function(req,res) {
+/* app.get('/index', function(req,res) {
 	fs.readFile('index.html', function(err, html) {
 	if(err){throw err;}
 	res.writeHead(200, {'Content-Type': 'text/html'})
@@ -42,7 +71,7 @@ app.get('/index', function(req,res) {
             res.end()
 	})
 
-})
+}) */
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
