b03a2aa67e778771e82c333af929b75311aca3b3
Baptiste-Bo <baptbonneau@yahoo.fr>
2019-11-05 10:46:04 +0100

    'API_pays'

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-var express = require("express");
+var express = require("express");/* npm install express */
+var csv = require('csv-express')/* npm install csv-express*/
+
+const fs = require('fs')
+
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+
+app.get('/', function (req, res) {
+    res.send('Hello, vous êtes à la racine de ce serveur ! allez voir /index')
+  })
+
+//API
+
+/* app.get('/user/:name', function(req, res) {
+	var age=''+req.query.age;
+	if(age!=="undefined" && age.trim().length){
+	res.send('Hello '  + req.params.name + ' tu as ' + age +' ans');
+	}else{
+	(res.send('Hello '  + req.params.name));
+	}
+}) */
+
+app.get('/names', function(req,res) {
+	res.format({
+        'application/json': function () {
+            res.json([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
+        },
+
+        'application/csv': function () {
+            res.csv([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
+        }
+    })
+})
+
+//static ressources
+
+app.get('/index', function(req,res) {
+	fs.readFile('index.html', function(err, html) {
+	if(err){throw err;}
+	res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(html)
+            res.end()
+	})
+
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  });
+
+
