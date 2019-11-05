395e897b622413b7a16bed8e79c21c4267405899
brocero <alexia.brocero@yahoo.fr>
2019-11-05 10:10:41 +0100

    API recettes

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-var express = require("express");
-var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+var http = require("https");
+
+var options = {
+	"method": "GET",
+	"hostname": "recipe-puppy.p.rapidapi.com",
+	"port": null,
+	"path": "/?i=banana%",
+	"headers": {
+		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+		"x-rapidapi-key": "bfbccef416msh5d68796c71ff54ep1d47b5jsnd820a352027f"
+	}
+};
+
+var req = http.request(options, function (res) {
+	var chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+	
+	
 });
+
+req.end();
+
+
+
