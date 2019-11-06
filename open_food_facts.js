221edfb02c2078b9926aebbd9de4e5fb8f0cdd75
brocero <alexia.brocero@yahoo.fr>
2019-11-06 09:10:54 +0100

    API open food facts

--- open_food_facts.js
+++ open_food_facts.js
@@ -1,16 +1,18 @@
-var http = require("https");
+var https = require("https");
+
+// requête pour origins='france'
 
 var options = {
 	"method": "GET",
-	"hostname": "fr.openfoodfacts.org/cgi",
+	"hostname": "fr.openfoodfacts.org",
 	"port": null,
-	"path": "search.pl?search_terms=%27france%27&json=1&page_size=500",
+	"path": "/cgi/search.pl?search_terms=&origins=france&json=1&page_size=5",
 	"headers": {
-		"x-rapidapi-host": "fr.openfoodfacts.org/cgi"
+		"x-rapidapi-host": "fr.openfoodfacts.org"
 	}
 };
 
-var req = http.request(options, function (res) {
+var req = https.request(options, function (res) {
 	var chunks = [];
 
 	res.on("data", function (chunk) {
@@ -27,3 +29,35 @@ var req = http.request(options, function (res) {
 
 req.end();
 
+
+// ############################################################
+
+
+// requête pour origins='France'
+
+var options = {
+	"method": "GET",
+	"hostname": "fr.openfoodfacts.org",
+	"port": null,
+	"path": "/cgi/search.pl?search_terms=&origins=France&json=1&page_size=5",
+	"headers": {
+		"x-rapidapi-host": "fr.openfoodfacts.org"
+	}
+};
+
+var req = https.request(options, function (res) {
+	var chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+	
+	
+});
+
+req.end();
