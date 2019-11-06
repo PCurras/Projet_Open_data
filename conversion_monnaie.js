ae643ce22812419cba04d3044f8c758c13e2002c
Baptiste-Bo <baptbonneau@yahoo.fr>
2019-11-06 19:12:17 +0100

    'merge_2_APIs'

---
+++ conversion_monnaie.js
@@ -0,0 +1,59 @@
+var https = require("https");
+
+var options_convert = {
+	"method": "GET",
+	"hostname": "currency-exchange.p.rapidapi.com",
+	"port": null,
+	"path": "/exchange?q=1.0&from=EUR&to=AOA",
+	"headers": {
+		"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+		"x-rapidapi-key": "efb3b66297msh599418614da6f08p158ab5jsnc9958e4c2520"
+	}
+};
+
+https.get(options_convert, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+  res.on('error', (e) => {
+	console.error(e);
+});
+
+const sc=res.statusCode.toString();
+
+function test(statusCode) {
+  if (sc.startsWith('2')== true) {
+    return "OK";
+  } else {
+    return "NOT ok";
+  }
+}
+
+console.log(test(res.statusCode));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var csv = require("csv-express");
 var http = require("https");
 const fs = require("fs");
+var fetchUrl = require("fetch").fetchUrl;
 
 var options = {
 	"method": "GET",
@@ -15,6 +16,92 @@ var options = {
 	}
 };
 
+app.get('/', function (req, res) {
+    res.send('Hello, vous êtes à la racine de ce serveur ! allez voir /index')
+  })
+
+		var currency = "";
+		var change = "";
+app.get('/currency/:country' , function(req,res){
+	 
+	   
+		var name_country = req.params.country;
+
+		var http = require("https");
+
+		var options = {
+			"method": "GET",
+			"hostname": "restcountries-v1.p.rapidapi.com",
+			"port": null,
+			"path": "/name/"+name_country,
+			"headers": {
+				"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+				"x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+			}
+		};
+
+		var req1 = http.request(options, function (res1) {
+			var chunks = [];
+			res1.on("data", function (chunk) {
+				chunks.push(chunk);
+			});
+
+			res1.on("end", function () {
+				var body = Buffer.concat(chunks);
+						
+				var json = JSON.parse(body.toString());
+				var fullbody = json.map(function(data){ return (data) })[0]
+				console.log(fullbody['currencies'][0],fullbody['population'],fullbody['translations']['fr']);
+				currency = fullbody['currencies'][0];
+				
+				
+				//**************************************************************************************
+					var http2 = require("https");
+
+					var options2 = {
+						"method": "GET",
+						"hostname": "currency-exchange.p.rapidapi.com",
+						"port": null,
+						"path": "/exchange?q=1.0&from=EUR&to="+currency,
+						"headers": {
+							"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+							"x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+						}
+					};
+
+					var req2 = http2.request(options2, function (res2) {
+						var chunks = [];
+
+						res2.on("data", function (chunk) {
+							chunks.push(chunk);
+						});
+
+						res2.on("end", function () {
+							var body = Buffer.concat(chunks);
+							
+							console.log(body.toString());
+							change = body.toString();
+							
+							res.send('Vous avez choisi le pays suivant : '  + name_country + '. Le code de la monnaie de ce pays est le suivant : ' + currency + '. La valeur de la conversion est de ' + change);
+					
+						});
+});
+
+					req2.end();
+				
+				
+				
+				
+				
+				
+				//*****************************************************************************************
+
+		
+			});
+		});
+		req1.end();
+		})
+
 app.get('/index', function(req,res) {
 	fs.readFile('index.html', function(err, html) {
 		if(err){
@@ -26,41 +113,11 @@ app.get('/index', function(req,res) {
 		res.write(html)
 		res.end()
 	})
-
 })
 
-var req = http.request(options, function (res) {
-	var chunks = [];
 
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		var body = Buffer.concat(chunks);
-		console.log( 'detail:', body.toString());
- 		var json = JSON.parse(body);
-		var code = json.map(function(data){ return (data['currencies']) });
-		console.log('monnaie du pays :',code);
-		app.get('/names', function(req,res) {
-			res.format({
-				'application/json': function () {
-
-					res.json(code);
-				},
-
-				'application/csv': function () {
-					res.csv(code);
-				}
-			})
-		})
-	});
-
-
-});
-
-req.end();
 
 app.listen(3000, () => {
 	console.log("Server running on port 3000");
 })
+
