0b6e000c318ae665741889a1349c0bf0e7b4d8dd
manon-dekyvere <44434925+manon-dekyvere@users.noreply.github.com>
2019-11-07 18:27:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var cors = require('cors')
+
 var express = require("express");
 var app = express();
 const port = process.env.PORT || 3000
+app.use(cors());
 
 require('./routes.js')(app, express);
 
