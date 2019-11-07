5c442d1baabed1a7e6fa576137fe31f9ab7cd518
brocero <alexia.brocero@yahoo.fr>
2019-11-07 19:18:22 +0100

    gestion des ereurs csv

--- routes.js
+++ routes.js
@@ -1,77 +1,84 @@
-// librairies
+const fs = require("fs");
 var csv = require("csv-express");
 var http = require("https");
-const fs = require("fs");
 var fetchUrl = require("fetch").fetchUrl;
+const Json2csvparser = require('json2csv').Parser
 
 module.exports = function(app, express) {
     const routes = express.Router();
 
     //TODO FUNCTIONS
 
-    //ROUTES
+    function callAPIfromCountryName(name_country, res) {
+        var http = require("https");
 
-	// route accès à la racine
-	app.get('/', function (req, res) {
-		res.send('Hello, vous êtes à la racine de ce serveur ! allez voir /index')
-	  })
+        var options = {
+            "method": "GET",
+            "hostname": "restcountries-v1.p.rapidapi.com",
+            "port": null,
+            "path": "/name/" + name_country,
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+            }
+        };
+        res.format({
+            'application/json': function () {
+                var req1 = http.request(options, function (res1) {
+                    var chunks = [];
+                    res1.on("data", function (chunk) {
+                        chunks.push(chunk);
+                    });
 
-	var currency = "";
-	var change = "";
-	var sc="";
-			
-	// route accès au pays
-	app.get('/currency/:country' , function(req,res){
-		 
-	// récupération du pays (actuellement écrit dans l'url, à terme : sera récupéré par ce qu'entre le client dans la barre de recherche)
-		var name_country = req.params.country;
+                    res1.on("end", function () {
+                        var body = Buffer.concat(chunks);
 
-	// API des infos pays 
-		var options = {
-			"method": "GET",
-			"hostname": "restcountries-v1.p.rapidapi.com",
-			"port": null,
-			"path": "/name/"+name_country, // récupération de la variable name_country
-			"headers": {
-				"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-				"x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
-			}
-		};
+                        var json = JSON.parse(body.toString());
+                        var fullbody = json.map(function (data) {
+                            return (data)
+                        })[0]
+                        console.log(fullbody['currencies'][0], fullbody['population'], fullbody['translations']['fr']);
+                        currency = fullbody['currencies'][0];
 
-		res.format({
-			'application/json': function(){
-			// récupération du résultat de la requête sur l'API
-				var req1 = http.request(options, function (res1) {
-					var chunks = [];
-					res1.on("data", function (chunk) {
-						chunks.push(chunk);
-					});
+                        addConversionMoney(fullbody, res);
 
-					res1.on("end", function () {
-						var body = Buffer.concat(chunks);
+                    });
+                });
+                req1.end();
+            },
+            'text/csv': function () {
+                var req1 = http.request(options, function (res1) {
+                    var chunks = [];
+                    res1.on("data", function (chunk) {
+                        chunks.push(chunk);
+                    });
 
-			// selection de l'affichage des résultats
-						var json = JSON.parse(body.toString());
-						var fullbody = json.map(function(data){ return (data) })[0]
-						console.log(fullbody['currencies'][0],fullbody['population'],fullbody['translations']['fr']);
-						currency = fullbody['currencies'][0];
+                    res1.on("end", function () {
+                        var body = Buffer.concat(chunks);
+
+                        var json = JSON.parse(body.toString());
+                        var fullbody = json.map(function (data) {
+                            return (data)
+                        })[0]
+                        console.log(fullbody['currencies'][0], fullbody['population'], fullbody['translations']['fr']);
+                        currency = fullbody['currencies'][0];
+
+
+                        //**************************************************************************************
+                        var http2 = require("https");
+
+                        var options2 = {
+                            "method": "GET",
+                            "hostname": "currency-exchange.p.rapidapi.com",
+                            "port": null,
+                            "path": "/exchange?q=1.0&from=EUR&to="+currency,
+                            "headers": {
+                                "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+                                "x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+                            }
+                        };
 						
-						
-						//**************************************************************************************
-							
-			// API conversion de la monnaie
-						var options2 = {
-							"method": "GET",
-							"hostname": "currency-exchange.p.rapidapi.com",
-							"port": null,
-							"path": "/exchange?q=1.0&from=EUR&to=ABC",
-							"headers": {
-								"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
-								"x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
-							}
-						};
 
-			// récupération du résultat de la requête 		
 						http.get(options2, (res2) => {
 							 console.log('statusCode:', res2.statusCode);
 							 //console.log('headers:', res2.headers);
@@ -81,41 +88,134 @@ module.exports = function(app, express) {
 							  if (sc.startsWith('2')== true) {
 									res2.on('data', (resultat) => {
 								var change = resultat.toString();
-								//res.send('Vous avez choisi le pays suivant : '  + name_country + '. Le code de la monnaie de ce pays est le suivant : ' + currency + '. La valeur de la conversion est de ' + change);
-								fullbody.conversionMoney = change;
-                                res.send(fullbody);
+								
+						        fullbody.conversionMoney = change;
+                                fields = ["name", "topLevelDomain", "alpha2Code", "alpha3Code", "callingCodes", "capital", "altSpellings", "region", "subregion", "population", "latlng", "demonym", "area", "gini", "timezones", "borders", "nativeName", "numericCode", "currencies", "languages", "translations", "relevance", "conversionMoney"]
+
+                                json2csvParser = new Json2csvparser({fields})
+                                csv = json2csvParser.parse(fullbody, function (err) {
+                                    res.redirect('/')
+                                })
+                                res.setHeader('Content-disposition', 'attachment; filename=test.csv')
+                                res.set('Content-Type', 'text/csv')
+                                res.send(csv)
 							});
 							  } else {
-								  fullbody.conversionMoney = "not available";
-                                res.send(fullbody);
+						        fullbody.conversionMoney = "not available";
+                                fields = ["name", "topLevelDomain", "alpha2Code", "alpha3Code", "callingCodes", "capital", "altSpellings", "region", "subregion", "population", "latlng", "demonym", "area", "gini", "timezones", "borders", "nativeName", "numericCode", "currencies", "languages", "translations", "relevance", "conversionMoney"]
+
+                                json2csvParser = new Json2csvparser({fields})
+                                csv = json2csvParser.parse(fullbody, function (err) {
+                                    res.redirect('/')
+                                })
+                                res.setHeader('Content-disposition', 'attachment; filename=test.csv')
+                                res.set('Content-Type', 'text/csv')
+                                res.send(csv)
 							  }
 						});
-							
-					});
-				});
+						
+                        // var req2 = http2.request(options2, function (res2) {
+                            // var chunks = [];
 
-			req1.end();	
-		   }
+                            // res2.on("data", function (chunk) {
+                                // chunks.push(chunk);
+                            // });
+
+                            // res2.on("end", function () {
+                                // var body = Buffer.concat(chunks);
+
+                                // console.log(body.toString());
+                                // change = body.toString();
+
+                                // res.send('Vous avez choisi le pays suivant : '  + name_country + '. Le code de la monnaie de ce pays est le suivant : ' + currency + '. La valeur de la conversion est de ' + change);
+                                // fullbody.conversionMoney = change;
+                                // fields = ["name", "topLevelDomain", "alpha2Code", "alpha3Code", "callingCodes", "capital", "altSpellings", "region", "subregion", "population", "latlng", "demonym", "area", "gini", "timezones", "borders", "nativeName", "numericCode", "currencies", "languages", "translations", "relevance", "conversionMoney"]
+
+                                // json2csvParser = new Json2csvparser({fields})
+                                // csv = json2csvParser.parse(fullbody, function (err) {
+                                    // res.redirect('/')
+                                // })
+                                // res.setHeader('Content-disposition', 'attachment; filename=test.csv')
+                                // res.set('Content-Type', 'text/csv')
+                                // res.status(200).send(csv)
+                            // });
+                        // });
+
+                        // req2.end();
+                    });
+                });
+                req1.end();
+            }
         })
-	});
+    }
 
+    function addConversionMoney(fullbody, res) {
+        var http = require("https");
 
-	app.get('/index', function(req,res) {
-		fs.readFile('index.html', function(err, html) {
-			if(err){
-				res.writeHead(500, err.message)
-				res.end()
+        var options3 = {
+            "method": "GET",
+            "hostname": "currency-exchange.p.rapidapi.com",
+            "port": null,
+            "path": "/exchange?q=1.0&from=EUR&to="+currency,
+            "headers": {
+                "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+                "x-rapidapi-key": "26e57b845amshaa9422739e19bd5p1d003djsnbd617bf3b072"
+            }
+        };
+
+		// récupération du résultat de la requête 		
+		http.get(options3, (res2) => {
+			console.log('statusCode:', res2.statusCode);
+			//console.log('headers:', res2.headers);
+						
+			sc=res2.statusCode.toString();
+
+			if (sc.startsWith('2')== true) {
+				res2.on('data', (resultat) => {
+					var change = resultat.toString();
+					//res.send('Vous avez choisi le pays suivant : '  + name_country + '. Le code de la monnaie de ce pays est le suivant : ' + currency + '. La valeur de la conversion est de ' + change);
+					fullbody.conversionMoney = change;
+					res.send(fullbody);
+				});
 			} else {
-				res.writeHead(200, {'Content-Type': 'text/html'})
+				fullbody.conversionMoney = "not available";
+				res.send(fullbody);
 			}
-			res.write(html)
-			res.end()
-		})
-	})
+		});
+	}
+		
+    //ROUTES
 
-// apply the routes to our application
-	app.use('/', routes);
+    app.get('/country/:country', function (req, res) {
+        var name_country = req.params.country;
+        callAPIfromCountryName(name_country, res);
+    })
+
+    app.get('/', function (req, res) {
+        fs.readFile('client.html', function (err, html) {
+            if (err) {
+                res.writeHead(500, err.message)
+                res.end()
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+            }
+            res.write(html)
+            res.end()
+        })
+    })
+    app.get('/', function (req, res) {
+        fs.readFile("stylesheet.css", function (err, css) {
+            if (err) {
+                res.writeHead(500, err.message);
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+            }
+            res.write(css);
+            res.end();
+        })
+    })
+
+    // apply the routes to our application
+    app.use('/', routes);
 }
-
-
-
